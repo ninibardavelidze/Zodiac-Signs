9e45370e2f81098d64584d727b30db3df9dfca70
ninibardavelidze <96244633+ninibardavelidze@users.noreply.github.com>
2021-12-19 20:54:01 +0400

    Add files via upload

---
+++ web.js
@@ -0,0 +1,82 @@
+var zodiacArray = [
+    {
+        zodiacName: "aries",
+        image: ("src", "https://i.insider.com/5b2971615e48ec14098b45b0?width=800&format=jpeg&auto=webp"),
+        personality:"The symbol for this sign is that of a ram for good reason. Aries is full of life and possesses high energy, which makes this sign always looking for a challenge. The typical Aries person is full of vitality, curiosity, and has a heightened sense of justice. They often excel at anything where a bit of competition is involved, whether this is of a cognitive nature or physical. The challenge is the thing. If there is a chance to win, to best someone, to show off abilities, Aries is in it to win it. The Aries person feels most alive when they are in control and leading others. They are often impatient and abrupt with those whom they feel are not their equals and are in a leadership position. Most Aries do not like being told what to do if the person in charge is less talented than they."
+    },
+    {
+        zodiacName: "taurus",
+        image: ("src", "https://i.insider.com/5b2971615e48ec7f088b45b4?width=800&format=jpeg&auto=webp"),
+        personality: "The Taurus symbol is the bull because this signs characteristic is to be peaceful and methodical. This sign is often very deliberate in their actions, relaxed, and enjoys all of the sensual pleasures that abound in this dimension. Food, drink, sex, luxury, are all sought after by the typical taruean. Because this sign loves luxury they are willing to work hard to obtain it. They are slow to anger, but once disturbed it can be volatile. Taurus signs look for stability in their lives and in their partners and co-workers. Taurus people are not in a hurry, typically, and it can take them many years to decide what it is that they would like to do for a living, or where they’d like to go on vacation. Many of this sign prefer to live in the country as they are drawn to the earth and the natural world. Earth signs tend to have solid, reputable natures, which coincides with the Earth."
+    },
+    {
+        zodiacName: "gemini",
+        image: ("src","https://i.insider.com/5b2971615e48ec43008b46da?width=800&format=jpeg&auto=webp"),
+        personality: "The Gemini symbol is that of a pair of twins. This is because this sign rarely likes to do anything alone. Gemini are never happier than when they are sharing ideas and concepts with someone else. Communication is a key element for this sign, so many of this sign go into occupations that include some communication in some large capacity. Curiosity is a key characteristic of this sign, and they are people persons. Gemini are great at parties because they can find almost anything to talk about with anyone. Gemini are adventurous by nature and so engage in traveling as often as they can as it affords them the opportunity to meet new people, experience new ideas, and to learn new concepts."
+    },
+    {
+        zodiacName: "cancer",
+        image: ("src","https://i.insider.com/5b2971615e48ec1e008b46eb?width=800&format=jpeg&auto=webp"),
+        personality: "The crab symbolizes this sign for good reason. Just as the crab carries its own shell on its back, so too, does the typical cancerian…so to speak. The typical cancer person is all about home and family and is very dedicated and loyal to friends and family. Crabs often move sideways instead of moving in a straight line, and so will the person of this sign. Often, if it will avoid a fight, or achieve the goal, then moving in alternative directions to arrive at the destination is typical. They tend to grasp and hold tightly those things that make them happy, and never let go. The key characteristic of this sign is that Cancer people need to be needed. They need to know that they matter to someone and that they are secure in that love. Brave, courageous, protective, all describe the best possible traits of this sign. Shy, reserved, brooding, and moody, do not serve the cancer person well, but may surface if their needs are not being met"
+    },
+    {
+        zodiacName: "leo",
+        image: ("src", "https://i.insider.com/5b2971615e48ec4c008b46db?width=800&format=jpeg&auto=webp"),
+        personality: "The lion is the symbol for this sign because the lion embodies the best characteristics of the leo; proud, regal, relaxed, and in charge. Leo people know they are in charge, even when it may seem outwardly that they are not. Leo signs tend to like relaxation, preferably someplace warm and comfortable. Leo likes the big picture, not the small details and fine print. Things that are too complicated, involved, or boring, they have no patience for. They are natural leaders and don’t often do well in situations where they have to take orders from others. Love is king to a leo. To love and be loved is a driving motivation for this sign. Leos, not unlike the lion of a pride, need to be adored. If they are ignored or go unrecognized for some contribution they will feel hurt. All leos like to know that they have been seen and appreciated."
+    },
+    {
+        zodiacName: "virgo",
+        image: ("src", "https://i.insider.com/5b2971615e48ec1e008b45e5?width=800&format=jpeg&auto=webp"),
+        personality: "The cool, calm, clarity that this sign exudes is why the symbol of the Maiden was chosen. Virgo people are mild mannered on the surface, but underneath there is a flurry of activity. Their minds are never quiet; always thinking, calculating, assessing. They loves making something out of nothing, nurturing and growing small things. They tend to be extremely detail oriented and particularly like producing something that is not only useful, but beautiful and skillful. Virgo don’t mind being alone as long as they know that they are appreciated and needed. They are sympathetic listeners, and are ready to help out those in need. Their good nature often makes them easy to take advantage of, and often they end up taking on more jobs and tasks than they should. This sign would do well to carefully consider the situation and the task before agreeing to take it on."
+    },
+    {
+        zodiacName: "libra",
+        image: ("src", "https://i.insider.com/5b2971615e48ec42008b45e5?width=800&format=jpeg&auto=webp"),
+        personality: "Libra people tend to need balance in their lives like no other sign of the zodiac, which makes the symbol of the scales particularly appropriate for this sign. Libra people need to keep a balance between work lives and recreational lives and an equal balance in their emotional and spiritual/physical lives. Because of this need Libra signs can sometimes seem wishy-washy when they are asked to make a decision. This is because they must ‘weigh’ all of their options. However, rest assured, that when a Libra makes up their mind the decision is likely to be the best win/win for all involved; benefiting the most people. This sign does not like to see people unhappy."
+    },
+    {
+        zodiacName: "scorpio",
+        image: ("src","https://i.insider.com/5b2971615e48ec20008b46d5?width=800&format=jpeg&auto=webp"),
+        personality: "The symbol for Scorpio is the scorpion. This creature was chosen because it typifies many of the characteristics of this sign. The scorpio person is not necessarily aggressive on its own, unless provoked and in general would rather be contemplative. They crave alone time and often become extremely annoyed when they don’t get it. Scorpio people are great secret keepers and feel each emotion more intensely than other signs. Perhaps because of this they are able to be quite discerning when assisting people with their problems. They are able to cut to the bottom line and show others the best and the worst in their lives"
+    },
+    {
+        zodiacName: "sagittarius",
+        image: ("src","https://i.insider.com/5b2971615e48ec1c008b46eb?width=800&format=jpeg&auto=webp"),
+        personality: "Those born under this sign will love travelling, discovering new things, meeting new people, and learning new things. Sagittarians crave the freedom of the open road, both metaphorically and physically. They are most unhappy when they are tied to a normal routine and can become restless in situations where there is not enough variety involved. Because they travel so much and are eager to experience new things, those of this sign are fun to be around and often have large numbers of friends. They question everything and think that everyone should do the same."
+    },
+    {
+        zodiacName: "capricorn",
+        image: ("src","https://i.insider.com/5b2971615e48ec21008b46f4?width=800&format=jpeg&auto=webp"),
+        personality: "The mountain goat is the symbol for this sign because of the characteristic of the goat to climb higher and higher without losing his footing. Capricorn people are the type that are goal oriented and driven to succeed despite all odds. They will work long and hard hours toward a purposeful goal. Self-disciplined and successful, it is no wonder that some of the world’s greatest scientists, leaders, and teachers have been Capricorn"
+    },
+    {
+        zodiacName: "aquarius",
+        image: ("src","https://i.insider.com/5b2971615e48ec1a008b4700?width=800&format=jpeg&auto=webp"),
+        personality: "Fascinated by all things gadget, many Aquarians enjoy tinkering with inventions and processes. On the surface, Aquarius people are often not very emotional, because they are more concerned with the exchanging of ideas and how they might better assist someone in their cause. Aquarians have a great deal of patience and tolerance for their fellow man. They make excellent teachers and relief aid workers, though do not become personally involved with them."
+    },
+    {
+        zodiacName: "pisces",
+        image: ("src","https://i.insider.com/5b2971615e48ec23008b46e7?width=800&format=jpeg&auto=webp"),
+        personality: "They are tremendously dedicated individuals, staying with companies for decades where others move from one job to the next. So, too, with relationships. Others may find Pisces difficult to get to know because they don’t often readily share their underlying passion and compassion. Pisces signs tend to be highly and deeply religious, and sometimes will go overboard with their devotion to their particular faith. This can also lead to intolerance and bigotry."
+    },
+    ]
+    function getInfo(){
+        var zodiacType = document.getElementById('zodiacType').value.toLowerCase() 
+        
+    
+        for (i = 0; i < zodiacArray.length; i++){
+            if (zodiacType === zodiacArray[i].zodiacName){
+                document.getElementById("zSign").textContent = zodiacArray[i].zodiacName
+                document.getElementById("zImage").src = zodiacArray[i].image
+                document.getElementById("zPersonality").textContent = zodiacArray[i].personality
+                return
+            } else {
+                    document.getElementById("zSign").textContent = "That's not an appropriate answer. Please try again!"
+                    document.getElementById("zImage").src = ""
+                    document.getElementById("zPersonality").textContent = ""
+    
+            }
+        }
+    }
+    
+    
